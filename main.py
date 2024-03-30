84ceee7af4ed7f152532e3742526bfc7e48bbad9
Arham Mahajan <mahajanarham0@gmail.com>
2024-03-30 18:23:46 +0530

    added concatenation, stacking, splitting methods

--- main.py
+++ main.py
@@ -114,3 +114,22 @@ import numpy as np #np is an alias
 
 # print(a.swapaxes(0,1)) # swaps the specified axes, useful when we are working with large dimensional arrays, in this case it does the same as transpose 
 
+# a1 = np.array([[1,2,3,4,5],
+#             [6,7,8,9,10]])
+# a2= np.array([[11,12,13,14,15],
+#             [16,17,18,19,20]])
+
+#a = np.concatenate((a1,a2), axis=0) # rowwise concatenation 
+#a = np.concatenate((a1,a2), axis=1) # columnwise concatenation 
+
+#a = np.stack((a1,a2)) # joins the arrays adding a new axis(dimension)
+#a = np.vstack((a1,a2)) # rowwise concatenation in this case 
+#a = np.hstack((a1,a2)) # columnwise concatenation in this case
+
+a = np.array([[1,2,3,4,5,6],
+               [7,8,9,10,11,12],
+               [13,14,15,16,17,18],
+               [19,20,21,22,23,24]])
+
+print(np.split(a, 4, axis=0)) # rowwise splitting  
+print(np.split(a,2,axis=1)) #columnwise splitting 
